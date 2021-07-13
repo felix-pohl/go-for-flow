8f21fe3fdf94f5d1328743503545503cbc52b592
Felix.Pohl <pohl.felix89@web.de>
2021-07-13 09:51:34 +0200

    initiler commit mit starter

---
+++ src/test/java/com/example/demo/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
