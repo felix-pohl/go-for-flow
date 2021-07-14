7f168ffeb6b926ef339da877fc7b1e3bb39cff1c
Ak1r4 <peter@hildel.com>
2021-07-14 16:05:09 +0200

    Meteoritencontroller ergänzt

--- src/main/java/com/example/demo/SchiffDTO.java
+++ src/main/java/com/example/demo/SchiffDTO.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
 public class SchiffDTO {
     private int power=100;
-    
+
+
 }
