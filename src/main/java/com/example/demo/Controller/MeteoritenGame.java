610b6f3d402ba9c935fce4e273e589e0a193fda9
René Laborge <rene.laborge@web.de>
2021-08-13 14:07:45 +0200

    Eingabe auswerten

--- src/main/java/com/example/demo/Controller/MeteoritenGame.java
+++ src/main/java/com/example/demo/Controller/MeteoritenGame.java
@@ -24,13 +24,22 @@ public class MeteoritenGame {
     public ModelAndView meteoriten(){
         ModelAndView mav = new ModelAndView("meteoriten");
         mav.addObject("schiffpower", schiff.getPower());
+        mav.addObject("textfarbe", "rot");
+        mav.addObject("farbwort", "blau");
         return mav;
     }
 
     @PostMapping("meteoriten")
-    public ModelAndView meteoritenAuswichen(@RequestParam("meteoriten_farbe") String meteoritenFarbe) {
+    public ModelAndView meteoritenAuswichen(@RequestParam("meteoriten_farbe") String meteoritenFarbe,
+                                            @RequestParam("farbwort") String farbwort) {
+        ModelAndView mav = new ModelAndView("meteoriten");
         System.out.print(meteoritenFarbe);
-        
-        return null;
+        System.out.print(farbwort);
+        if(!meteoritenFarbe.equals(farbwort)) {
+            schiff.setPower(schiff.getPower()-10);
+            mav.addObject("schiffpower", schiff.getPower());
+        }
+
+        return mav;
     }
 }
